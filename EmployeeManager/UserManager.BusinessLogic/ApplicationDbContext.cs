d2ae8c54cb8cd815e4056ae37e26322cd47208ca
Emir Veledar <emir.v@onedigital.group>
2024-04-23 19:58:29 +0200

    Session 4 :: EF Core Introduction

---
+++ EmployeeManager/UserManager.BusinessLogic/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManager.BusinessLogic.Entities;
+
+namespace UserManager.BusinessLogic;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+
+    public DbSet<Department> Departments { get; set; }
+}
